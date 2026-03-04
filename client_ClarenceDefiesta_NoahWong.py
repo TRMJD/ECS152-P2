2b37adca26413ecc9f9c8ca5c27cc32d64b5b719
CJ <cldefiesta@ucdavis.edu>
2026-03-03 21:42:47 -0800

    started client socket object

--- client_ClarenceDefiesta_NoahWong.py
+++ client_ClarenceDefiesta_NoahWong.py
@@ -3,6 +3,10 @@
 import socket
 import json
 
+HOST =  'localhost'
+PORT = 8080
 # create client socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to proxy server
+client_socket.connect((HOST, PORT))
+
