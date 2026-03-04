e6a48a1f428f7a52b921d134fdbdebbbba14dbb7
CJ <cldefiesta@ucdavis.edu>
2026-03-03 21:42:03 -0800

    updated host and port

--- client_ClarenceDefiesta_NoahWong.py
+++ client_ClarenceDefiesta_NoahWong.py
@@ -1,1 +1,8 @@
+# client 
+# sends messages to proxy server and awaits response
+import socket
+import json
 
+# create client socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to proxy server
