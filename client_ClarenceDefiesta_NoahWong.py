93b9d4dbc1d53f124b1656d65f34a1e325cb2a95
CJ <cldefiesta@ucdavis.edu>
2026-03-04 17:15:47 -0800

    client implementation complete

--- client_ClarenceDefiesta_NoahWong.py
+++ client_ClarenceDefiesta_NoahWong.py
@@ -3,10 +3,38 @@
 import socket
 import json
 
-HOST =  'localhost'
-PORT = 8080
+PROXYHOST = "127.0.0.1"
+PROXYPORT = 8080
+
 # create client socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to proxy server
-client_socket.connect((HOST, PORT))
+client_socket.connect((PROXYHOST, PROXYPORT))
 
+# send message to proxy server in JSON format
+def send_message(server_ip: str, server_port: int, message: str):
+    # check if message is 4 characters long
+    if len(message) != 4:
+        print("Error: Message must be 4 characters long")
+        return None
+    # create JSON payload with server_ip, server_port, and message
+    payload = {
+        "server_ip": server_ip,
+        "server_port": server_port,
+        "message": message
+    }
+    # new connection per request
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((PROXYHOST, PROXYPORT)) # connect to proxy server
+        client_socket.sendall(json.dumps(payload).encode("utf-8")) # send JSON payload to proxy server
+        # receive response from proxy server
+        response = client_socket.recv(4096).decode("utf-8")
+        print(f"Response from Proxy: {response}")
+        return response
+
+# main loop that reiterates sending messages
+while True:
+    server_ip = input("Enter server IP address")  
+    server_port = input("Enter server port: ")
+    message = input("Enter 4 character message: ")
+    send_message(server_ip, int(server_port), message)
